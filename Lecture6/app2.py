d605a526508685e77c38a1f49a5e45d775b5f346
Filip Szymanski <fszymanski@gmail.com>
2024-12-31 09:45:57 -0800

    Example of a single page app

---
+++ Lecture6/app2.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index2.html")
+
+texts = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+         "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+         "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
+
+@app.route("/first")
+def first():
+    return texts[0]
+
+@app.route("/second")
+def second():
+    return texts[1]
+
+@app.route("/third")
+def third():
+    return texts[2]
+
