53b35e2f8707c300f64d036d9bfb2929de4fb3a3
Filip Szymanski <fszymanski@gmail.com>
2025-01-06 17:12:36 -0800

    Added user authentication to airline application

--- Lecture7/airline/airline/urls.py
+++ Lecture7/airline/airline/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('flights.urls')),
+    path('', include('users.urls')),
+    path('flights/', include('flights.urls')),
     path('admin/', admin.site.urls),
 ]
